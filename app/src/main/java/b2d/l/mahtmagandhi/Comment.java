50d97f9258fa56aa4434838e3281402e8e4a8966
ashok <ashokkumar.fun@gmail.com>
2020-12-31 11:24:19 +0530

    need image array from problem page to comment page

--- app/src/main/java/b2d/l/mahtmagandhi/Comment.java
+++ app/src/main/java/b2d/l/mahtmagandhi/Comment.java
@@ -6,8 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,6 +20,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import org.json.JSONArray;
@@ -52,6 +57,9 @@ public class Comment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+        final SliderView sliderView = findViewById(R.id.imageSlider);
+
+        final SliderAdapterExample adapter = new SliderAdapterExample(this);
 //        StatusBarUtil.setTransparent(this);
         avi = findViewById(R.id.avi);
         /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -70,8 +78,8 @@ public class Comment extends AppCompatActivity {
         RecyclerView.Adapter adapter = new CommentAdapter(this, data);
         recyclerView.setAdapter(adapter);*/
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        passToken = getIntent().getBooleanExtra("passToken",false);
-        if(passToken){
+        passToken = getIntent().getBooleanExtra("passToken", true);
+        if (passToken) {
             Toast.makeText(this, "comming from problem", Toast.LENGTH_SHORT).show();
         }
         String url = Url.baseurl + getIntent().getStringExtra("url");//"/ctalk_comments";
@@ -87,6 +95,7 @@ public class Comment extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 try {
                     if (response.getBoolean("success")) {
+                        Log.d("ashok_comment", response.toString());
                         ArrayList<CommentData> commentData = new ArrayList<>();
                         JSONArray data = response.getJSONArray("data");
                         for (int i = 0; i < data.length(); i++) {
@@ -94,8 +103,24 @@ public class Comment extends AppCompatActivity {
                             CommentData comment = gson.fromJson(data.getJSONObject(i).toString(), CommentData.class);
                             commentData.add(comment);
                         }
-                        RecyclerView.Adapter adapter = new CommentAdapter(Comment.this, commentData, preferences.getInt(Datas.id, 1));
-                        recyclerView.setAdapter(adapter);
+                        RecyclerView.Adapter commentAdapter = new CommentAdapter(Comment.this, commentData, preferences.getInt(Datas.id, 1));
+                        recyclerView.setAdapter(commentAdapter);
+
+                        JSONArray data_images = response.getJSONArray("data_images");
+
+                        for (int i = 0; i < data_images.length(); i++) {
+                            adapter.addItem(new SliderItem(Url.burl + data_images.getJSONObject(i).getString("image_url")));
+
+                        }
+                        sliderView.setSliderAdapter(adapter);
+
+                        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+                        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+                        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+                        sliderView.setIndicatorSelectedColor(Color.WHITE);
+                        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+                        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+                        sliderView.startAutoCycle();
                     } else {
                         Toast.makeText(Comment.this, "" + response.getString("message"), Toast.LENGTH_SHORT).show();
                         //login page
